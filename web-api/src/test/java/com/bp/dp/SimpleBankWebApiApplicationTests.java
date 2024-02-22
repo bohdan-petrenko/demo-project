5e29973fd7cf9478fbb886b142c311f750893439
Bohdan Petrenko <bohdan.petrenko@gmail.com>
2024-02-22 14:02:01 +0200

    Rename project to simple-bank

--- web-api/src/test/java/com/bp/dp/SimpleBankWebApiApplicationTests.java
+++ web-api/src/test/java/com/bp/dp/SimpleBankWebApiApplicationTests.java
@@ -1,11 +1,10 @@
 package com.bp.dp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 //todo cant create context because mongo not configured for tests
 //@SpringBootTest
-class DemoProjectApplicationTests {
+class SimpleBankWebApiApplicationTests {
 
 	@Test
 	void contextLoads() {
